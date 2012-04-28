03015192d1c4baccf4f019588ea483b695d0d715
Antti Vikman <antti.vikman@gmail.com>
2012-04-28 07:43:50 +0300

    Added some Quantum gates, now supporting following gates (in parentheses is the name to be used in QASM files): Identity gate (i) Hadamard gate (h) Pauli-X gate (x) Pauli-Y gate (y) Pauli-Z gate (z) Phase shift gates (P<n>), where <n> is divider Swap gate (swap) Controlled NOT gate (cnot) Toffoli gate (toffoli) Fredkin gate (fredkin) Controlled phase shift gates (cP<n>), where <n> is divider

--- app/models/QuantumGates.scala
+++ app/models/QuantumGates.scala
@@ -26,22 +26,46 @@ object QuantumGates {
     "x" -> DenseMatrix((Complex(0,0),Complex(1,0)),
                        (Complex(1,0),Complex(0,0))),
 
+    "y" -> DenseMatrix((Complex(0,0),Complex(0,-1)),
+                       (Complex(0,1),Complex(0,0))),
+
+    "z" -> DenseMatrix((Complex(1,0),Complex(0,0)),
+                       (Complex(0,0),Complex(-1,0))),
+
     "swap" -> DenseMatrix((Complex(1,0),Complex(0,0),Complex(0,0),Complex(0,0)),
                           (Complex(0,0),Complex(0,0),Complex(1,0),Complex(0,0)),
                           (Complex(0,0),Complex(1,0),Complex(0,0),Complex(0,0)),
-                          (Complex(0,0),Complex(0,0),Complex(0,0),Complex(1,0)))
+                          (Complex(0,0),Complex(0,0),Complex(0,0),Complex(1,0))),
+
+    "cnot" -> DenseMatrix((Complex(1,0),Complex(0,0),Complex(0,0),Complex(0,0)),
+                          (Complex(0,0),Complex(1,0),Complex(0,0),Complex(0,0)),
+                          (Complex(0,0),Complex(0,0),Complex(0,0),Complex(1,0)),
+                          (Complex(0,0),Complex(0,0),Complex(1,0),Complex(0,0))),
+
+    "toffoli" -> DenseMatrix((Complex(1,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0)),
+                             (Complex(0,0),Complex(1,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0)),
+                             (Complex(0,0),Complex(0,0),Complex(1,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0)),
+                             (Complex(0,0),Complex(0,0),Complex(0,0),Complex(1,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0)),
+                             (Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(1,0),Complex(0,0),Complex(0,0),Complex(0,0)),
+                             (Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(1,0),Complex(0,0),Complex(0,0)),
+                             (Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(1,0)),
+                             (Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(1,0),Complex(0,0))),
+
+    "fredkin" -> DenseMatrix((Complex(1,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0)),
+                             (Complex(0,0),Complex(1,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0)),
+                             (Complex(0,0),Complex(0,0),Complex(1,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0)),
+                             (Complex(0,0),Complex(0,0),Complex(0,0),Complex(1,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0)),
+                             (Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(1,0),Complex(0,0),Complex(0,0),Complex(0,0)),
+                             (Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(1,0),Complex(0,0)),
+                             (Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(1,0),Complex(0,0),Complex(0,0)),
+                             (Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(0,0),Complex(1,0)))
   )
 
-  /**
-   * x to power of y
-   * @param x from
-   * @param y power
-   * @return sum
-   * @todo implement and then refactor to more suitable place
-   */
-  implicit def pow(x: Complex, y: Complex): Complex = {
-    x
-  }
+  //TODO: Refactor all of these to somewhere else
+  implicit def arg(x: Complex) = math.atan2(x.imag, x.real)
+  implicit def exp(x: Complex) = Complex(math.exp(x.real) * math.cos(x.imag), math.exp(x.real) * math.sin(x.imag))
+  implicit def log(x: Complex) = Complex(math.log(Complex.scalar.norm(x)), arg(x))
+  implicit def pow(x: Complex, y: Complex): Complex = exp(y*log(x))
 
   /**
    * Returns the DenseMatrix format of the requested gate
@@ -50,7 +74,22 @@ object QuantumGates {
    */
   def get(key: String): DenseMatrix[Complex] = {
     // Quantum Fourier transform (QFT) relies on controlled phase gates that are usually named in QASM cPn
-    if(key.startsWith("cP")) {
+    if (key.startsWith("P")) {
+      if(key.drop(2).forall(_.isDigit)) {
+        val phase: Complex = (
+          key.drop(2).toInt match {
+            case 2 =>
+              Complex(-1,0)
+            case 4 =>
+              Complex(0,1)
+            case n: Int =>
+              pow(Complex(math.E, 0), Complex(0, 2*math.Pi/n))  // e^(2*i*pi/n)
+          }
+          )
+        return DenseMatrix((Complex(1,0),Complex(0,0)),
+                           (Complex(0,0),phase))
+      }
+    } else if(key.startsWith("cP")) {
       if(key.drop(2).forall(_.isDigit)) {
       val phase: Complex = (
         key.drop(2).toInt match {
